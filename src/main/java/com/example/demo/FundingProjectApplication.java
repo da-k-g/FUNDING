86dca0d7ed2ff84aaa014c8dad92098f45c93eb7
VM <VM@VM-1.mshome.net>
2024-12-02 10:54:24 +0900

    project

---
+++ src/main/java/com/example/demo/FundingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundingProjectApplication.class, args);
+	}
+
+}
